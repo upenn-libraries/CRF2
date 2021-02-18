da93ba249be6f2b6c3cf1addff6adf6680917524
Andrew Kimball <andrewnk@upenn.edu>
2021-02-18 14:37:36 +0000

    Adds humanize

--- crf2/settings.py
+++ crf2/settings.py
@@ -88,6 +88,7 @@ INSTALLED_APPS = [
     'django.contrib.admindocs',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.humanize',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
